c0175eb9d3e309993f18a1b3c024a341c2ce3919
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-02 15:48:48 +0200

    my first commit this December

---
+++ prac.js
@@ -0,0 +1,14 @@
+let person ={
+
+    name : "Nothemba",
+    surname: "Tsheme",
+    
+}
+
+
+let response = "";
+for (let i in person){
+    response += person[i] + " ";
+
+}
+console.log(response)
